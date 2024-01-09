0b240e95f09a91b8805f5b6f28a5d934388ba4bb
andyadc <andaicheng@qq.com>
2024-01-09 16:30:49 +0800

    marketing

--- src/test/java/com/andyadc/marketing/test/MapperTests.java
+++ src/test/java/com/andyadc/marketing/test/MapperTests.java
@@ -1,16 +1,28 @@
 package com.andyadc.marketing.test;
 
 import com.andyadc.marketing.persistence.entity.Award;
+import com.andyadc.marketing.persistence.entity.StrategyAward;
 import com.andyadc.marketing.persistence.mapper.AwardMapper;
+import com.andyadc.marketing.persistence.mapper.StrategyAwardMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class MapperTests {
 
     @Autowired
     private AwardMapper awardMapper;
+    @Autowired
+    private StrategyAwardMapper strategyAwardMapper;
+
+    @Test
+    public void test_StrategyAwardMapper_queryByStrategyId() {
+        List<StrategyAward> strategyAwardList = strategyAwardMapper.queryByStrategyId(1);
+        System.out.println(strategyAwardList.size());
+    }
 
     @Test
     public void test_AwardMapper_selectByPrimaryKey() {
